0762749bc568544cdc69a32ade3a2f8c25d199d9
Yury Chajkou <yury80@gmail.com>
2018-10-03 23:15:01 +0300

    Advanced zeros calculator implemented. Not suitable for bases, which consist of duplicate sequences of multiplaers.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let i = 0;
+  let startDivider = 2;
+  let simpleMultiplier = [];
+  let exponent = [];
+  let expCount = 1;
+
+  while (startDivider < base) {
+    if (base % startDivider == 0) {
+      simpleMultiplier[i] = startDivider;
+      i++;
+      base = base / startDivider;
+    } else {
+      startDivider++;
+    }
+
+  };
+  simpleMultiplier[i] = startDivider;
+
+   function compareSimpleMultiplier(a, b) {
+    return a + b;
+  }
+  simpleMultiplier.sort(compareSimpleMultiplier);
+   {
+    let ZerosCount = 0;
+    while (number > 0) {
+      number = Math.trunc(number / simpleMultiplier[0]);
+      ZerosCount = ZerosCount + number;
+
+    }
+    return ZerosCount;
+
+  }
 }
